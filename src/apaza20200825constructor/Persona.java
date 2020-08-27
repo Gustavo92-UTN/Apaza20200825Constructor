26e79e38114fa4cd991f4d6e4172722c5296924e
Gustavo92-UTN <gustavoapazah@gmail.com>
2020-08-26 21:13:20 -0300

    se agrego this()

--- src/apaza20200825constructor/Persona.java
+++ src/apaza20200825constructor/Persona.java
@@ -1,27 +1,30 @@
 package apaza20200825constructor;
 public class Persona {
-    int DNI = 0;
-    String nombre = "", apellido = "";
+    int DNI;
+    String nombre, apellido;
 //Constructor por defecto
     public Persona(){
         System.out.println("Creando persona...");
+        DNI = 0;
+        nombre = "";
+        apellido = "";
     }
 //Constructor que reciba DNI
     public Persona(int DNI){
+        this();
         this.DNI = DNI;
     }
 //Constructor que reciba DNI, Nombre
     public Persona(int DNI, String nombre){
-        this.DNI = DNI;
+        this(DNI);
         this.nombre = nombre;
     }
 //Constructor que reciba DNI, Nombre y Apellido
     public Persona(int DNI, String nombre, String apellido){
-        this.DNI = DNI;
-        this.nombre = nombre;
+        this(DNI,nombre);
         this.apellido = apellido;
     }
-//Método que muestre todos los datos de la persona.
+//Método que muestre por pantalla todos los datos de la persona.
     public void mostrarPorPantalla(){
         if(DNI != 0){
             System.out.println(this.DNI);
